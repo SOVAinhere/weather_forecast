8909c4637844013d9f606a7b52cdd248ae92c7cd
sova45 <sovainhere@yandex.ru>
2025-05-28 21:11:33 +0300

    //

--- weather_project/weather/views.py
+++ weather_project/weather/views.py
@@ -1,32 +1,36 @@
 import requests
 from django.shortcuts import render
+
+
 def home(request):
     city = request.GET.get('city')
     weather_data = None
 
     if city:
-        geo_url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1&language=ru&format=json"
+        geo_url = (
+            f"https://geocoding-api.open-meteo.com/v1/search"
+            f"?name={city}&count=1&language=ru&format=json"
+        )
         geo_response = requests.get(geo_url).json()
 
-        if geo_response.get("results"):
-            lat = geo_response["results"][0]["latitude"]
-            lon = geo_response["results"][0]["longitude"]
-            name = geo_response["results"][0]["name"]
+        results = geo_response.get("results")
+        if results:
+            lat = results[0]["latitude"]
+            lon = results[0]["longitude"]
+            name = results[0]["name"]
 
             weather_url = (
-                f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}"
-                f"&current_weather=true"
+                f"https://api.open-meteo.com/v1/forecast"
+                f"?latitude={lat}&longitude={lon}&current_weather=true"
             )
             weather_response = requests.get(weather_url).json()
+
             weather_data = {
                 "city": name,
                 "temperature": weather_response["current_weather"]["temperature"],
-                "windspeed": weather_response["current_weather"]["windspeed"]
+                "windspeed": weather_response["current_weather"]["windspeed"],
             }
         else:
-            weather_data = {
-                "error": "Город не найден"
-            }
+            weather_data = {"error": "Город не найден"}
 
-    return render(request, "weather.html", {"weather": weather_data, 'city': city})
-
+    return render(request, "weather.html", {"weather": weather_data, "city": city})
