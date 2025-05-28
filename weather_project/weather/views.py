8b73d623b12e6b3379e48563735010e77a0431bd
sova45 <sovainhere@yandex.ru>
2025-05-29 00:22:25 +0300

    //

--- weather_project/weather/views.py
+++ weather_project/weather/views.py
@@ -1,47 +1,37 @@
 import requests
 from django.shortcuts import render
 
+API_KEY = "260ccc8107bc73f8c1c977fd9e0eb86e" #делаю через openweathermap
 
 def home(request):
     city = request.GET.get("city")
     weather_data = None
 
     if city:
-        geo_data = fetch_geolocation(city)
-        if geo_data:
-            weather = fetch_weather(*geo_data[:2])  # lat, lon
-            if weather:
+        url = "https://api.openweathermap.org/data/2.5/weather"
+        params = {
+            "q": city,
+            "appid": API_KEY,
+            "units": "metric",
+            "lang": "ru"
+        }
+
+        try:
+            response = requests.get(url, params=params)
+            data = response.json()
+
+            if response.status_code == 200:
                 weather_data = {
-                    "city": geo_data[2],  # name
-                    "temperature": weather["temperature"],
-                    "windspeed": weather["windspeed"],
+                    "city": data["name"],
+                    "temperature": data["main"]["temp"],
+                    "windspeed": data["wind"]["speed"],
+                    "description": data["weather"][0]["description"],
+                    "icon": data["weather"][0]["icon"],
+                    "main": data["weather"][0]["main"],
                 }
             else:
-                weather_data = {"error": "Не удалось получить погоду"}
-        else:
-            weather_data = {"error": "Город не найден"}
+                weather_data = {"error": f"Ошибка: {data.get('message', 'Неизвестная ошибка')}"}
+        except Exception as e:
+            weather_data = {"error": "Не удалось получить данные о погоде."}
 
     return render(request, "weather.html", {"weather": weather_data, "city": city})
-
-
-def fetch_geolocation(city):
-    url = "https://geocoding-api.open-meteo.com/v1/search"
-    params = {"name": city, "count": 1, "language": "ru", "format": "json"}
-    try:
-        response = requests.get(url, params=params).json()
-        result = response.get("results")
-        if result:
-            return result[0]["latitude"], result[0]["longitude"], result[0]["name"]
-    except Exception:
-        pass
-    return None
-
-
-def fetch_weather(lat, lon):
-    url = "https://api.open-meteo.com/v1/forecast"
-    params = {"latitude": lat, "longitude": lon, "current_weather": "true"}
-    try:
-        response = requests.get(url, params=params).json()
-        return response.get("current_weather")
-    except Exception:
-        return None
